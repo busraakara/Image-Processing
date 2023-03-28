788c72b4ae43737feaea6b734561c4390cfb0ea4
busraakara <105877486+busraakara@users.noreply.github.com>
2023-03-28 14:44:37 +0300

    Update laplacian_filter.py

--- laplacian_filter.py
+++ laplacian_filter.py
@@ -1,21 +1,21 @@
+#Import libraries
 import math 
 import numpy as np
 import cv2 
 
-img = cv2.imread("img.png")
+#Upload image
+img = cv2.imread("image.jpg")
 cv2.imshow("Original image",img)
-print(img.shape)
 
-
+#Convert BGR colorful image to gray scale
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray image", img) 
-print(img.shape)
 
 genislik=img.shape[1]
 yukseklik=img.shape[0] 
 
-#Bu 2 kernel da Laplacian Filter için kullanılır
-kernel= [[-1, -1, -1], 
+#These 2 kernels used for Laplacian filter
+kernel1= [[-1, -1, -1], 
         [-1, 8, -1], 
         [-1, -1, -1]]
 
@@ -25,20 +25,23 @@ kernel2= [[0, 1, 0],
 
 laplacian_filtre = np.zeros(shape=(yukseklik, genislik))
 
-for i in range (0,yukseklik-2):
-    for j in range (0,genislik-2):
-        block = img[i:i+3 , j:j+3]
-        d = np.sum(np.multiply(block,kernel1))
-        if(d<0):
-            d=0
-        elif(d>255):
-            d=255   
-        laplacian_filtre[i+1][j+1]=d
-              
-laplacian_filtre=np.uint8(laplacian_filtre)        
-cv2.imshow("laplacian filtrelenmis resim",laplacian_filtre)    
+#Zero padding
+laplacian = np.zeros((yukseklik+2,genislik+2))
+laplacian[1:yukseklik+1 , 1:genislik+1] = img
 
+#Convolution
+for i in range (0,yukseklik):
+    for j in range (0,genislik):
+        block = laplacian[i:i+3 , j:j+3]
+        d = np.sum(np.multiply(block,kernel1))
+        laplacian_filtre[i][j]=d
+        
+#Normalization
+minn = np.amin(laplacian_filtre)
+maxx = np.amax(laplacian_filtre)
+laplacian_filtre = ((laplacian_filtre-maxx)/(maxx-minn)*255.0)              
+laplacian_filtre=np.uint8(laplacian_filtre)        
+cv2.imshow("laplacian filtred image",laplacian_filtre)    
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()    
-
