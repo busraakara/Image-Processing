5fe5f3cd4d4521d496d506381f7faff67c92396f
busraakara <105877486+busraakara@users.noreply.github.com>
2023-03-28 14:22:12 +0300

    Update sobel_filter.py

--- sobel_filter.py
+++ sobel_filter.py
@@ -16,6 +16,7 @@ Luminosity method : b * 0.114 + g * 0.587 + r * 0.299
 """
 b , g , r = img[:,:,0] , img[:,:,1] , img[:,:,2]
 img = np.uint8(np.round((b * 0.114 + g * 0.587 + r * 0.299)))
+
 cv2.imshow("Gray Scaled Image",img)
 
 #define kernels        
@@ -33,45 +34,29 @@ kernel_y = [[-1, -2, -1],
 height=img.shape[0]
 width=img.shape[1]
 
-sobel_filter_x = np.zeros(shape=(height, width))
-sobel_filter_y = np.zeros(shape=(height, width))
+#zero padding
+sobel=np.zeros((height+2,width+2))
+sobel[1:height+1,1:width+1] = img
+
 sobel_filter = np.zeros(shape=(height, width))
 
 #use convolution for filter
-for i in range (0,height-2):
-    for j in range (0,width-2):
-           
-        block = img[i:i+3 , j:j+3]  
-
+for i in range (0,height):
+    for j in range (0,width):
+        block = sobel[i:i+3 , j:j+3]  
         convolution_x=np.sum(np.multiply(block,kernel_x))
         convolution_y=np.sum(np.multiply(block,kernel_y))
-        sobel_filter[i+1][j+1]= np.sqrt(convolution_x**2 + convolution_y**2) 
-           
-        #some numbers may not be between 0-255 so equalization is required.   
-        if (sobel_filter[i+1][j+1]<0):
-            sobel_filter[i+1][j+1]=0
-        elif (sobel_filter[i+1][j+1]>255):
-            sobel_filter[i+1][j+1]=255
-                      
-        #this part is for visualize x and y filters   
-        if (convolution_x<0):
-            convolution_x=0
-        elif (convolution_x>255):
-            convolution_x=255
-        sobel_filter_x[i+1][j+1]= convolution_x
+        sobel_filter[i][j]= np.sqrt(convolution_x**2 + convolution_y**2)
 
-        if (convolution_y<0):
-            convolution_y=0
-        elif (convolution_y>255):
-            convolution_y=255
-        sobel_filter_y[i+1][j+1]= convolution_y
-        
-sobel_filter_x=np.uint8(sobel_filter_x) 
-sobel_filter_y=np.uint8(sobel_filter_y)
-sobel_filter=np.uint8(sobel_filter)       
-   
-cv2.imshow("Sobel x Filtred Image",sobel_filter_x)        
-cv2.imshow("Sobel y Filtred Image",sobel_filter_y) 
+#apply normalization to filtered image
+#because after convolution some pixel values are above 255. 
+#We have to scale image values between 0-255.
+minn = np.amin(sobel_filter)
+maxx = np.amax(sobel_filter)
+sobel_filter = ((sobel_filter-maxx)/(maxx-minn)*255.0)
+#this equation creates floating point values. 
+#We have to change image type to 8 bit unsigned integer.
+sobel_filter=np.uint8(sobel_filter)
 cv2.imshow("Sobel Filtred Image",sobel_filter) 
   
 cv2.waitKey(0)
